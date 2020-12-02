4ebba14f2ade84063a81d43c05823bb85f4e4254
syugoudada <to154514asa134009@gmail.com>
2020-12-02 19:04:24 +0900

    ページ遷移

---
+++ Top_Page/top_page.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>intelli_base</title>
+</head>
+
+<body>
+  <header>
+    <div class="header_contents">
+      <div class="serach_bar">
+        <input type="text" id="search_bar" placeholder="入力してください">
+        <input type="submit" id="submit" name="submit" value="ボタン">
+      </div>
+      <div class="user_tag">
+        <div class="login_tag">
+          <a href="../Login/login_form.php">こんにちは、ログイン</a></form>
+        </div>
+        <div id="cart_tag">
+          <a href="#">カート</a>
+        </div>
+      </div>
+    </div>
+    </div>
+  </header>
+
+  <div class="contents">
+
+  </div>
+
+  <footer>
+
+  </footer>
+
+</body>
+
+</html>
