7d46f3232d90799d33daaadc83fb600e2868e79f
tingtt <t.t.launcher.real@gmail.com>
2021-01-30 02:06:20 +0900

    テストデータ用のDBを使用するように変更

--- includes/db_config.php
+++ includes/db_config.php
@@ -2,4 +2,5 @@
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'password');
 define('DB_HOST', 'mysql');
-define('DB_NAME', 'intelli_base');
+// define('DB_NAME', 'intelli_base');
+define('DB_NAME', 'intelli_base_version2');
