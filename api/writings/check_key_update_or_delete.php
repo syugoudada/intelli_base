2fbfc0cf4678dba37df839cbf55238f7d3b92c89
tingtt <t.t.launcher.real@gmail.com>
2021-02-05 19:53:23 +0900

    add : api for check key updated or share rejected.

---
+++ api/writings/check_key_update_or_delete.php
@@ -0,0 +1,51 @@
+<?php
+
+$response = array();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' || isset($_GET['test'])) {
+
+    $response['error'] = false;
+    $response['message'] = '承認';
+
+    //値の取得
+    if (isset($_POST['account_id']) && isset($_POST['local_writing_id']) && isset($_POST['book_id']) && isset($_POST['share_key'])) {
+        $account_id = $_POST['account_id'];
+        $local_writing_id = $_POST['local_writing_id'];
+        $book_id = $_POST['book_id'];
+        $share_key = $_POST['share_key'];
+    } else if (isset($_GET['account_id']) && isset($_GET['local_writing_id']) && isset($_GET['book_id']) && isset($_GET['share_key'])) {
+        $account_id = $_GET['account_id'];
+        $local_writing_id = $_GET['local_writing_id'];
+        $book_id = $_GET['book_id'];
+        $share_key = $_GET['share_key'];
+    } else {
+        $account_id = 518;
+        $local_writing_id = 1;
+        $book_id = 260;
+        $share_key = '';
+        $response['error'] = true;
+        $response['message'] = 'post parameter error';
+    }
+
+    //DBファイルの操作
+    require_once '../../includes/DbOperation.php';
+    $db = new DbOparation();
+
+    $value = $db->select('SELECT id FROM writings WHERE share_key = ?', [$share_key]);
+    if (!$value) {
+        // 一致する共有キーがなかった場合
+        if (!$db->select('SELECT id FROM writings WHERE account_id = ? AND local_writing_id = ? AND book_id = ?', [$account_id, $local_writing_id, $book_id])) {
+            // 共有キーと日付情報意外が一致するデータがあった場合（共有キーがアップデートされた）
+            $response['content'] = ['status' => 'shareRejected'];
+        } else {
+            $response['content'] = ['status' => 'keyChanged'];
+        }
+    } else {
+        $response['content'] = ['status' => 'noChange'];
+    }
+} else {
+    $response['error'] = true;
+    $response['messgae'] = 'あなたは承認されていません';
+}
+
+echo json_encode($response);
