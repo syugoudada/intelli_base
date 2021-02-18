04788f67138bcf4d52d2e32e011f09f8ec6ca14a
tingtt <t.t.launcher.real@gmail.com>
2021-02-18 12:02:29 +0900

    change : Change parameter for updateing date log.

--- api/writings/udpate_writings.php
+++ api/writings/udpate_writings.php
@@ -8,15 +8,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' || isset($_GET['test'])) {
     $response['message'] = '承認';
 
     //値の取得
-    if (isset($_POST['account_id']) && isset($_POST['local_writing_id'])) {
-        $account_id = $_POST['account_id'];
-        $local_writing_id = $_POST['local_writing_id'];
-    } else if (isset($_GET['account_id']) && isset($_GET['local_writing_id'])) {
-        $account_id = $_GET['account_id'];
-        $local_writing_id = $_GET['local_writing_id'];
+    if (isset($_POST['share_key'])) {
+        $share_key = $_POST['share_key'];
+    } else if (isset($_GET['share_key'])) {
+        $share_key = $_GET['share_key'];
     } else {
-        $account_id = 518;
-        $local_writing_id = 1;
+        $share_key = 518;
         $response['error'] = true;
         $response['message'] = 'post parameter error';
     }
@@ -25,20 +22,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' || isset($_GET['test'])) {
     require_once '../../includes/DbOperation.php';
     $db = new DbOparation();
 
-    $value = $db->select('SELECT id FROM writings WHERE account_id = ? AND local_writing_id = ?', [$account_id, $local_writing_id]);
-    if (!$value) {
+    // 日付
+    $dateInt = (new DateTime())->format('YmdHis'); //-> yyyymmddhhiiss
+    // var_dump([$dateInt, $writing_id]);
+    if (!$db->insert('UPDATE writings SET update_date = ? WHERE share_key = ?', [$dateInt, $share_key])) {
         $response['error'] = true;
-        $response['message'] = 'Statement error on select';
-    } else {
-        // id
-        $writing_id = $value[0]['id'];
-        // 日付
-        $dateInt = (new DateTime())->format('YmdHis'); //-> yyyymmddhhiiss
-        var_dump([$dateInt, $writing_id]);
-        if (!$db->insert('UPDATE writings SET update_date = ? WHERE id = ?', [$dateInt, $writing_id])) {
-            $response['error'] = true;
-            $response['message'] = 'Statement error on update';
-        }
+        $response['message'] = 'Statement error on update';
     }
 
     $response['content'][0] = ['status' => !$response['error']];
